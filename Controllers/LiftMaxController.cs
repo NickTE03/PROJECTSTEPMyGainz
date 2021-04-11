2edc5a79703bf42651ddadea8668caac67725d49
NickTE03 <68023365+NickTE03@users.noreply.github.com>
2021-04-10 22:44:08 -0400

    Add files via upload

---
+++ Controllers/LiftMaxController.cs
@@ -0,0 +1,58 @@
+﻿using PROJECTSTEP101010.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace PROJECTSTEP101010.Controllers
+{
+    public class LiftMaxController : Controller
+    {
+        // Methods
+
+        // Create
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Add(LiftMax e)
+        {
+             return RedirectToAction("Details", e);
+        }
+
+        public IActionResult Details(LiftMax e)
+        {
+            return View(e);
+        }
+
+        [HttpGet]
+
+        public IActionResult Edit(int Id)
+        {
+            LiftMax e = new LiftMax();
+            e.Id = Id;
+            e.Date = null;
+            e.Name = "Bench Press";
+            e.Weight = 315;
+            return View(e);
+
+        }
+
+        [HttpPost]
+
+        public IActionResult Edit(LiftMax e)
+        {
+            return RedirectToAction("Details", e);
+        }
+
+       
+
+        // Read
+
+        // Update
+
+        // Delete
+    }
+}
---
+++ Models/LiftMax.cs
@@ -0,0 +1,26 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+
+namespace PROJECTSTEP101010.Models
+{
+    public class LiftMax
+    {
+        // Fields & Properties
+        [Required(ErrorMessage = "Enter your Id!")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Enter your Date!")]
+        public string Date { get; set; }
+
+        [Required(ErrorMessage = "Enter Name of Lift!")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Enter Weight Lifted!")]
+        public int Weight { get; set; }
+
+        // Constructors
+
+        // Methods
+    }
+}
